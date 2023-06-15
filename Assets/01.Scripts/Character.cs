4854d9d4538d8f30904e08e721b54e9a7cec0e50
ban324 <ban324@naver.com>
2023-06-15 19:56:58 +0900

    BASECOMMIT

--- Assets/01.Scripts/Character.cs
+++ Assets/01.Scripts/Character.cs
@@ -1,9 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using UnityEngine;
+
+public enum STATUS_DEBUFF
+{
+    NONE,//정상인
+    HANGNAIL,//거스러미가 나서 신경쓰인다. 공격력 절반
+    HEADACHE,//마뎀 / 2, 어질어질하다 그죠?
+    WET,//마방/2
+    MYALGIA,//근육통
+    DRUNK,//취해서 속도가 절반, =팔자걸음
+    POISON,//똥독
+    STUN,//기절!턴 1회 넘김
+    DEAD//말이 없다. 시체인 것 같다.
+}
+public enum STATUS_BUFF
+{
+    NONE,
+    ATK_UP,
+    MAG_UP,
+    DEF_UP,
+    MDEF_UP,
+    SPEED_UP
+    
+}
+
+public enum PHYSIC_TYPE
+{
+    SLASH,
+    SMASH,
+    STING,
+    BERSERK
+}
+
+public enum MAGIC_TYPE
+{
+    A,
+    B,
+    C,
+    MBERSERK
+}
 
 public abstract class Character 
 {
+    
     public string name;
 
     public int MaxHP;
@@ -18,6 +59,17 @@ public abstract class Character
     private int _speed;
 
     public StatusSO status;
+
+    public PHYSIC_TYPE current_PType;
+    public MAGIC_TYPE current_MType;
+
+    public PHYSIC_TYPE physics_Weakness;
+    public MAGIC_TYPE magic_Weakness;
+    
+    public PHYSIC_TYPE physics_Strength;
+    public MAGIC_TYPE magic_Strength;
+
+    
 
     public int Atk
     {
@@ -54,4 +106,46 @@ public abstract class Character
         _mdef = status.mdef;
         _speed = status.speed;
     }
+
+    public void  TakeDamage(PHYSIC_TYPE pType, MAGIC_TYPE mType,SKLL_TYPE sType, int damage)
+    {
+        damage *= 1 - (Def / (1 + Def));
+        switch (sType)
+        {
+            case SKLL_TYPE.PHYSIC:
+                if(pType == physics_Weakness)
+                {
+                    damage *= 2;
+                }else if(pType == physics_Strength)
+                {
+                    if (Random.Range(0, 100) <= (HP / MaxHP) * 100)
+                    {
+                        damage *= 3;
+                    }
+                    else damage /= 2;
+                }
+                break;
+            case SKLL_TYPE.MAGIC:
+                if (mType == magic_Weakness)
+                {
+                    damage *= 2;
+                }
+                else if (mType == magic_Strength)
+                {
+                    if (Random.Range(0, 100) <= (HP / MaxHP) * 100)
+                    {
+                        damage *= 3;
+                    }
+                    else damage /= 2;
+                }
+                break;
+            case SKLL_TYPE.BUFF:
+                break;
+            case SKLL_TYPE.DEBUFF:
+                break;
+        }
+        HP -= damage;
+    }
+
+    public Skill selectedSkill;
 }
--- Assets/01.Scripts/Skill.cs
+++ Assets/01.Scripts/Skill.cs
@@ -2,8 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SKLL_TYPE
+{
+    PHYSIC,
+    MAGIC,
+    BUFF,
+    DEBUFF
+}
 public abstract class Skill 
 {
+    public PHYSIC_TYPE current_PType;
+    public MAGIC_TYPE current_MType;
+    public int damage;
+    public void SetType(PHYSIC_TYPE pType, MAGIC_TYPE mType)
+    {
+        current_PType = pType;
+        current_MType = mType;
+    }
     public abstract void OnActive(List<Character> target, int damage = 0);
 
 }
